6717b4b9795d2d92d92a0670d3a15fc03f27a7f5
qiu <1804870417@qq.com>
2020-05-18 16:32:02 +0800

    更新1111

---
+++ mywebpack/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="hello">
+		<input type="text" v-model="msg" @keydown.enter="enter" />
+		<p class="red">{{toupper}}</p>
+		<child cmsg="阿西吧" @change="getval"></child>
+	</div>
+</template>
+
+<script>
+	import child from './child'
+	export default {
+		name: 'HelloWorld',
+		components: {
+			child
+		},
+		data() {
+			return {
+				msg: 'asd'
+			}
+		},
+		computed: {
+			toupper() {
+				return this.msg.toUpperCase()
+			}
+		},
+		methods: {
+			enter() {
+				alert(this.msg)
+			},
+			getval(val){
+				console.log(val)
+			}
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	.red {
+		color: red;
+	}
+</style>
---
+++ mywebpack/src/components/child.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="">
+		<p>{{cmsg}}</p>
+		<p><button @click="fn">click</button></p>
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		name:'child',
+		data(){
+			return{
+				zmsg:'子内容'
+			}
+		},
+		props:['cmsg'],
+		methods:{
+			fn(){
+				this.$emit('change',this.zmsg)
+			}
+		}
+	}
+</script>
+<style>
+	
+</style>
